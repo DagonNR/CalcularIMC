f7451750c73b59ab93ac42ce32b1e30ce756deef
Daniel Narvaez <ceinnova11550@innovaccion.mx>
2022-08-01 05:10:27 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Calcular Índice de masa corporal (IMC).
+El presente repositorio se realizó con el fin de poder conocer el Índice de masa corporal (IMC), de manera más sencilla, además de mejorar las habilidades del uso de Python
+
+---
+
+## Requisitos
+- Probado en Python 3.10 o superior.
+- Computadora con sistema operativo de "Windows", "MacOS" o "Linux".
+- Editor de texto, como [Visual Studio Code](https://code.visualstudio.com), para una mayor comodidad.
+
+---
+
+## Importante
+- Solo lo he probado en Python 3.10 o superior.
+- Es algo sencillo tanto de hacer como de usar, más que nada su finalidad de creación fue mejorar mis habilidades con Python.
+- La idea original fue sacada de un video de YouTube, pero lo realicé a mi manera.
+
+---
+
+## Como usar
+- Al iniciar te pedirá tu nombre.
+- Entonces te pedira tanto tu peso, como tu altura importante colocarlos en kg y mt respectivamente.
+- Al finalizar te arrojará un resultado dependiendo de los datos que hayas colocado.
---
+++ imc.py
@@ -0,0 +1,22 @@
+#Pedir datos
+nombre = input("Buenas tardes favor de ingresar su nombre: ")
+print("Muy bien " + nombre + ", ahora por favor ingresa lo que se te pida a continuación")
+peso = int(input("Ingrese su peso en kg (Ejemplo: 70): "))
+altura = float(input("Ingrese su altura en mt (Ejemplo: 1.70): "))
+
+#Formula del IMC
+def formulaIMC(peso, altura):
+    imc = peso / (altura * altura)
+    return imc
+
+imc = formulaIMC(peso, altura)
+
+#Dar resultado
+if imc < 19:
+    print("Tu IMC indica que entras en el rango de delgado." + " Este es tu IMC representado en número: " + str(imc))
+if imc >= 20 and imc < 26:
+    print("Tu IMC indica que entras en el rango de normal." + " Este es tu IMC representado en número: " + str(imc))
+if imc >= 26 and imc < 30:
+    print("Tu IMC indica que entras en el rango de sobrepeso." + " Este es tu IMC representado en número: " + str(imc))
+if imc >= 30:
+    print("Tu IMC indica que entras en el rango de obesidad." + " Este es tu IMC representado en número: " + str(imc))
